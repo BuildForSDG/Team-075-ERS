5744dbe26e2a32f19b0a0067148953adb7fd0ca0
Bee <ardelmbiplang@gmail.com>
2020-05-16 14:04:05 +0100

    [Refact]: Remove firebase OAuth file

---
+++ src/components/history/history.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const  History = () => {
+  return (
+    <div className="report">
+      <p>Total Reports</p>
+      <span>10</span>
+      <p>Total As victim</p>
+      <span>2</span>
+      <p>Total Report as Witness</p>
+      <span>8</span>
+    </div>
+  );
+}
+
+export default History;
+
