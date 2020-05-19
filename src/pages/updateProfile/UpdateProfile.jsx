9380c3d00286b3e06bf3387dd00db08c5bd0c680
Daniel Chukwurah <dnchukwurah@sidmach.com>
2020-05-19 18:57:04 +0100

    style: update-profile page

--- src/pages/updateProfile/UpdateProfile.jsx
+++ src/pages/updateProfile/UpdateProfile.jsx
@@ -9,22 +9,36 @@ class UpdateProfile extends Component{
         <h4>Update Profile</h4>
         <p>Update your profile information</p>
 
-        <img src="images/updateProfile.svg" alt="profile-pic"/>
+        <img src="images/profilePicture.svg" alt="profile-pic" width="150px" height="150px"/>
 
         <h5>Johnson Pearson</h5>
         <p>+234 803 1234 5678</p>
         <p>johnson@mail.com</p>
-        <form id="update-profile" action="/report-accident">
+
+        <form id="update-profile" action="/update-profile">
           <fieldset>
-            <input type="text" placeholder="Name"/>
-            <input type="text" placeholder="Phone number"/>
-            <input type="text" placeholder="Emergency contact name"/>
-            <input type="text" placeholder="Emergency contact number"/>
+            <div className="form-group">
+              <input type="text" className="form-control" placeholder="Name"/>
+            </div>
+
+           <div className="form-group">
+             <input type="text" className="form-control" placeholder="Phone number"/>
+           </div>
+
+          <div className="form-group">
+            <input type="text" className="form-control" placeholder="Emergency contact name"/>
+          </div>
+
+          <div className="form-group">
+            <input type="text" className="form-control" placeholder="Emergency contact number"/>
+          </div>
+
             <CustomButton className="btn-send">
               Update
             </CustomButton>
           </fieldset>
         </form>
+
       </section>
 
     );
