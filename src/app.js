687f0c451f654315a202e20ddb5acd58f6efd1eb
unknown <oseeronlan@gmail.com>
2020-05-19 16:14:35 +0100

    fix:remove background rings for homepage

--- src/app.js
+++ src/app.js
@@ -16,17 +16,25 @@ class App extends React.Component {
     return (
       <Router>
         <div className="App">
-          <Navbar login={this.props.user}/>
-          <div className="container">
-            <Switch>
-              <Route exact path="/" render={() => (this.props.sent ? <Redirect to="/feedback" /> : <HomePage />)} />
+          <Navbar login={this.props.user} />
+          <Switch>
+            <Route exact path="/" render={() => (this.props.sent ? <Redirect to="/feedback" /> : <HomePage />)} />
+            <div className="container">
               <Route exact path="/profile" component={UserProfile} />
               <Route exact path="/report-accident" component={ReportAccident} />
               <Route exact path="/feedback" component={Feedback} />
-              <Route exact path="/login" render={() => (this.props.user.login === 200 ? <Redirect to="/" /> : <Login />)} />
-              <Route exact path="/sign-up" render={() => (this.props.user.login === 200 ? <Redirect to="/" /> : <SignUp />)} />
-            </Switch>
-          </div>
+              <Route
+                exact
+                path="/login"
+                render={() => (this.props.user.login === 200 ? <Redirect to="/" /> : <Login />)}
+              />
+              <Route
+                exact
+                path="/sign-up"
+                render={() => (this.props.user.login === 200 ? <Redirect to="/" /> : <SignUp />)}
+              />
+            </div>
+          </Switch>
         </div>
       </Router>
     );
--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -5,16 +5,15 @@ import { connect } from 'react-redux';
 import { signUpUserStartAsync } from '../../redux/user/user.actions';
 
 import { ReactComponent as Line } from '../../assets/images/Line.svg';
-import { ReactComponent as Girl } from '../../assets/images/girl.svg';
-import { ReactComponent as Google } from '../../assets/images/google.svg';
+// import { ReactComponent as Girl } from '../../assets/images/girl.svg';
+// import { ReactComponent as Google } from '../../assets/images/google.svg';
 import { ReactComponent as Facebook } from '../../assets/images/facebook.svg';
 
 import './sign-up.css';
 //custom button component reused from report accident component
 
-
 class SignUp extends React.Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
@@ -26,19 +25,19 @@ class SignUp extends React.Component {
       password: '',
       confirmPassword: '',
       terms: ''
-    }
+    };
   }
 
   setLoginDetails = (event) => {
     const { name, value } = event.target;
-    this.setState((prevState, PrevProps) => ( {[name]: value} ));
-  }
+    this.setState((prevState, PrevProps) => ({ [name]: value }));
+  };
 
   handleSubmit = (event) => {
     event.preventDefault();
     const { name, email, phoneNo, emergencyName, emergencyPhone, password, confirmPassword, terms } = this.state;
     const { signUpUserStartAsync } = this.props;
-    if (password === confirmPassword && terms === 'on' ) {
+    if (password === confirmPassword && terms === 'on') {
       signUpUserStartAsync(name, email, phoneNo, emergencyName, emergencyPhone, password);
     }
     this.setState((prevState, prevProps) => ({
@@ -51,88 +50,85 @@ class SignUp extends React.Component {
       },
       password: '',
       terms: ''
-    }))
-  }
+    }));
+  };
 
   render() {
-
     return (
       <section>
         <h2 className="signup-title">Create An Account</h2>
 
         <div className="login-section">
-
           <form id="login" onSubmit={this.handleSubmit}>
-
             <fieldset>
-
               <div className="left">
-
-                <input name="name" 
-                  type="text" 
-                  className="user-details" 
+                <input
+                  name="name"
+                  type="text"
+                  className="user-details"
                   required
-                  placeholder="Name" 
+                  placeholder="Name"
                   onChange={this.setLoginDetails}
                 />
 
-                <input name="phoneNo" 
-                  type="number" 
-                  className="user-details" 
+                <input
+                  name="phoneNo"
+                  type="number"
+                  className="user-details"
                   required
                   placeholder="Phone number"
                   onChange={this.setLoginDetails}
                 />
 
-                <input name="email" 
-                  type="email" 
-                  className="user-details" 
+                <input
+                  name="email"
+                  type="email"
+                  className="user-details"
                   required
                   placeholder="Email address"
                   onChange={this.setLoginDetails}
                 />
 
-                <input name="emergencyName" 
-                  type="text" 
-                  className="user-details" 
+                <input
+                  name="emergencyName"
+                  type="text"
+                  className="user-details"
                   required
                   placeholder="Emergency Name"
                   onChange={this.setLoginDetails}
                 />
 
-                <input name="emergencyPhone" 
-                  type="text" 
-                  className="user-details" 
+                <input
+                  name="emergencyPhone"
+                  type="text"
+                  className="user-details"
                   required
                   placeholder="Emergency Phone"
                   onChange={this.setLoginDetails}
                 />
 
-                <input name="password" 
-                  type="password" 
-                  className="user-details" 
+                <input
+                  name="password"
+                  type="password"
+                  className="user-details"
                   required
                   placeholder="Password"
                   onChange={this.setLoginDetails}
                 />
 
-                <input name="confirmPassword" 
-                  type="password" 
-                  className="user-details" 
+                <input
+                  name="confirmPassword"
+                  type="password"
+                  className="user-details"
                   required
                   placeholder="Confirm Password"
                   onChange={this.setLoginDetails}
                 />
 
                 <CustomButton className="btn-send register-btn">Register</CustomButton>
-  
+
                 <div className="policy">
-                  
-                  <input type="checkbox" 
-                    id="checkbox" 
-                    name="terms" 
-                    onChange={this.setLoginDetails}
-                  />
+                  <input type="checkbox" id="checkbox" name="terms" onChange={this.setLoginDetails} />
 
                   <label>
                     By clicking continue you agree to our
@@ -140,25 +136,21 @@ class SignUp extends React.Component {
                     <b>Terms of Service and Privacy Policy</b>
                   </label>
                 </div>
-
               </div>
-
             </fieldset>
-
           </form>
-  
-          <div className="middle">
-            <Line className='divider' />
-            <p>Or</p>
-            <Line className='divider' />
-          </div>
-  
-          <div className="right">
 
+          <div className="middle">
+            <Line className="divider" />
+            <p>Or</p>
+            <Line className="divider" />
+          </div>
+
+          <div className="right">
             <p>Sign up with one of your social accounts</p>
 
             <CustomButton className="soc-btn">
-              <Facebook id="facebook"/>
+              <Facebook id="facebook" />
               <span>sign in with facebook</span>
             </CustomButton>
 
@@ -167,27 +159,26 @@ class SignUp extends React.Component {
               <img src="images/google.svg" alt="google icon" id="google" />
               <span>sign in with google</span>
             </CustomButton>
-
           </div>
 
           <img src="images/girl.svg" alt="girl" id="girl" />
-
         </div>
 
         <p className="prompt-msg">
-          Already have an account? 
-          <Link to="/login" className='link'> <b>Log In</b> </Link>
+          Already have an account?
+          <Link to="/login" className="link">
+            {' '}
+            <b>Log In</b>{' '}
+          </Link>
         </p>
-
       </section>
     );
   }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  signUpUserStartAsync: (name, email, phoneNo, emergencyContactName,
-    emergencyContactPhoneNo, password) => dispatch(signUpUserStartAsync(name, email, phoneNo, emergencyContactName,
-      emergencyContactPhoneNo, password))
-})
+  signUpUserStartAsync: (name, email, phoneNo, emergencyContactName, emergencyContactPhoneNo, password) =>
+    dispatch(signUpUserStartAsync(name, email, phoneNo, emergencyContactName, emergencyContactPhoneNo, password))
+});
 
 export default connect(null, mapDispatchToProps)(SignUp);
