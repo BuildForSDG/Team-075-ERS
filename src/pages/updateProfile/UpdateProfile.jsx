97823af1f73660de25b3334f1c57f5201e8c191c
Daniel Chukwurah <dnchukwurah@sidmach.com>
2020-05-22 16:54:09 +0100

    fix: update-profile and index css

--- src/pages/updateProfile/UpdateProfile.jsx
+++ src/pages/updateProfile/UpdateProfile.jsx
@@ -5,41 +5,44 @@ import './update-profile.css';
 class UpdateProfile extends Component{
   render(){
     return(
-      <section className="update-profile">
-        <h4>Update Profile</h4>
-        <p>Update your profile information</p>
+      <div className="container">
 
-        <img src="images/profilePicture.svg" alt="profile-pic" />
+        <section className="update-profile">
+          <h4>Update Profile</h4>
+          <p>Update your profile information</p>
 
-        <h5>Johnson Pearson</h5>
-        <p>+234 803 1234 5678</p>
-        <p>johnson@mail.com</p>
+          <img src="images/profilePicture.svg" alt="profile-pic" />
 
-        <form id="update-profile" action="/update-profile">
-          <fieldset>
+          <h5>Johnson Pearson</h5>
+          <p>+234 803 1234 5678</p>
+          <p>johnson@mail.com</p>
+
+          <form id="update-profile" action="/update-profile">
+            <fieldset>
+              <div className="form-group">
+                <input type="text" className="form-control" placeholder="Name"/>
+              </div>
+
             <div className="form-group">
-              <input type="text" className="form-control" placeholder="Name"/>
+              <input type="text" className="form-control" placeholder="Phone number"/>
             </div>
 
-           <div className="form-group">
-             <input type="text" className="form-control" placeholder="Phone number"/>
-           </div>
+            <div className="form-group">
+              <input type="text" className="form-control" placeholder="Emergency contact name"/>
+            </div>
 
-          <div className="form-group">
-            <input type="text" className="form-control" placeholder="Emergency contact name"/>
-          </div>
+            <div className="form-group">
+              <input type="text" className="form-control" placeholder="Emergency contact number"/>
+            </div>
 
-          <div className="form-group">
-            <input type="text" className="form-control" placeholder="Emergency contact number"/>
-          </div>
+              <CustomButton className="btn-send">
+                Update
+              </CustomButton>
+            </fieldset>
+          </form>
 
-            <CustomButton className="btn-send">
-              Update
-            </CustomButton>
-          </fieldset>
-        </form>
-
-      </section>
+        </section>
+      </div>
 
     );
   }
