29716ea2cc9f67920f8a645ea481bb511fac4336
Bee <ardelmbiplang@gmail.com>
2020-05-27 19:06:11 +0100

    chore: Modal
    Modal reused for feedback, update user profile and victims info, added error handling, redux setup for update profile

--- src/redux/updateProfile/updateProfile.actions.js
+++ src/redux/updateProfile/updateProfile.actions.js
@@ -1,39 +1,39 @@
 import ConstantsActionTypes from './updateProfile.constants';
 
-const updateUserProfileAsync = (name, phoneNo, emergencyName, emergencyPhone, id, token) => 
-(dispatch) => {
+const updateUserProfileAsync = (
+name, phoneNo, emergencyName, emergencyPhone, id, token) => (dispatch) => {
   dispatch({
     type: ConstantsActionTypes.UPDATE_USER_PROFILE_START
   });
-  const bearer = `Bearer ${token}`;
-  fetch(`https://emresys.herokuapp.com/api/auth/profile/${id}`, {
-    method: 'post',
-    headers: {
-      Authorization: bearer,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name,
-      phoneNo,
-      emergencyContact: {
-        name: emergencyName,
-        phoneNo: emergencyPhone
-    }
+    const bearer = `Bearer ${token}`;
+    fetch(`https://emresys.herokuapp.com/api/auth/profile/${id}`, {
+      method: 'post',
+      headers: {
+        Authorization: bearer,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        phoneNo,
+        emergencyContact: {
+          name: emergencyName,
+          phoneNo: emergencyPhone
+      }
+      })
     })
-  })
-    .then((response) => {
-      dispatch({
-        type: ConstantsActionTypes.UPDATE_USER_PROFILE_SUCCESS,
-        payload: response.status
+      .then((response) => {
+        dispatch({
+          type: ConstantsActionTypes.UPDATE_USER_PROFILE_SUCCESS,
+          payload: response.status
+        });
+        return response.json();
+      })
+      .catch((error) => {
+        dispatch({
+          type: ConstantsActionTypes.UPDATE_USER_PROFILE_FAILED,
+          payload: error.message
+        });
       });
-      return response.json();
-    })
-    .catch((error) => {
-      dispatch({
-        type: ConstantsActionTypes.UPDATE_USER_PROFILE_FAILED,
-        payload: error.message
-      });
-    });
-}
+};
 
 export default updateUserProfileAsync;
