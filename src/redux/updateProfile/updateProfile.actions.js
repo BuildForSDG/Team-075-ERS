41e73560514269142e482087e03e1843c2d435fb
Bee <ardelmbiplang@gmail.com>
2020-05-29 14:36:39 +0100

    fix: Bug Fix#52

--- src/redux/updateProfile/updateProfile.actions.js
+++ src/redux/updateProfile/updateProfile.actions.js
@@ -6,35 +6,35 @@ const updateUserProfileAsync = (
   dispatch({
     type: ConstantsActionTypes.UPDATE_USER_PROFILE_START
   });
-      const bearer = `Bearer ${token}`;
-      fetch(`https://emresys.herokuapp.com/api/auth/profile/${id}`, {
-        method: 'post',
-        headers: {
-          Authorization: bearer,
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          name,
-          phoneNo,
-          emergencyContact: {
-            name: emergencyName,
-            phoneNo: emergencyPhone
-        }
-        })
-      })
-        .then((response) => {
-          dispatch({
-            type: ConstantsActionTypes.UPDATE_USER_PROFILE_SUCCESS,
-            payload: response.status
-          });
-          return response.json();
-        })
-        .catch((error) => {
-          dispatch({
-            type: ConstantsActionTypes.UPDATE_USER_PROFILE_FAILED,
-            payload: error.message
-          });
-        });
+  const bearer = `Bearer ${token}`;
+  fetch(`https://emresys.herokuapp.com/api/auth/profile/${id}`, {
+    method: 'post',
+    headers: {
+      Authorization: bearer,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name,
+      phoneNo,
+      emergencyContact: {
+        name: emergencyName,
+        phoneNo: emergencyPhone
+      }
+    })
+  })
+  .then((response) => {
+    dispatch({
+      type: ConstantsActionTypes.UPDATE_USER_PROFILE_SUCCESS,
+      payload: response.status
+    });
+    return response.json();
+  })
+  .catch((error) => {
+    dispatch({
+      type: ConstantsActionTypes.UPDATE_USER_PROFILE_FAILED,
+      payload: error.message
+    });
+  });
 };
 
 export default updateUserProfileAsync;
