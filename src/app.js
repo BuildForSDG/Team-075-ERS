ed15500ff862ced32b4296171cea98ae5266a1c2
unknown <oseeronlan@gmail.com>
2020-06-04 19:45:32 +0100

    feat:create reusable sidebar and dashboard component

--- src/app.js
+++ src/app.js
@@ -15,51 +15,48 @@ import './App.css';
 import WithSpinner from './components/with-spinner/with-spinner';
 import ResponseUnitHomePage from './pages/responseUnitHomePage/responseUnitHomePage';
 import ResponseUnitSignUp from './components/responseUnitSignUp/SignUp';
+import Dashboard from './components/dashboard/Dashboard';
 
 class App extends React.Component {
   render() {
     // console.log(this.props)
     return (
-
       <Router>
         <div className="App">
           <Navbar />
-          {
-
-            (!this.props.isLoading) ?
-            (
-              <HandleError>
-                <Switch>
-                  <React.Fragment>
-                    <Route exact path="/" render={() => (this.props.sent ? <Redirect to="/feedback" /> : <HomePage />)} />
-                    <Route exact path="/profile" component={UserProfile} />
-                    <Route exact path="/report-accident" component={ReportAccident} />
-                    <Route exact path="/update-profile" component={UpdateProfile} />
-                    <Route exact path="/feedback" component={Feedback} />
-                    <Route exact path="/google-map" component={GoogleMap} />
-                    <Route exact path="/ers" component={ResponseUnitHomePage} />
-                    <Route exact path="/ers-sign-up" component={ResponseUnitSignUp} />
-                    <Route
-                      exact
-                      path="/login"
-                      render={() => (this.props.user.login === 200 ? <Redirect to="/" /> : <Login />)}
-                    />
-                    <Route
-                      exact
-                      path="/sign-up"
-                      render={() => (this.props.user.signup === 201 ? <Redirect to="/login" /> : <SignUp />)}
-                    />
-                  </React.Fragment>
-                </Switch>
-              </HandleError>
-            )
-            :
+          {!this.props.isLoading ? (
+            <HandleError>
+              <Switch>
+                <React.Fragment>
+                  <Route exact path="/" render={() => (this.props.sent ? <Redirect to="/feedback" /> : <HomePage />)} />
+                  <Route exact path="/profile" component={UserProfile} />
+                  <Route exact path="/report-accident" component={ReportAccident} />
+                  <Route exact path="/update-profile" component={UpdateProfile} />
+                  <Route exact path="/feedback" component={Feedback} />
+                  <Route exact path="/google-map" component={GoogleMap} />
+                  <Route exact path="/ers" component={ResponseUnitHomePage} />
+                  <Route exact path="/ers-sign-up" component={ResponseUnitSignUp} />
+                  <Route
+                    exact
+                    path="/login"
+                    render={() => (this.props.user.login === 200 ? <Redirect to="/" /> : <Login />)}
+                  />
+                  <Route
+                    exact
+                    path="/sign-up"
+                    render={() => (this.props.user.signup === 201 ? <Redirect to="/login" /> : <SignUp />)}
+                  />
+                  <Route path="/dashboard" component={Dashboard} />
+                </React.Fragment>
+              </Switch>
+            </HandleError>
+          ) : (
             <WithSpinner></WithSpinner>
-          }
-            </div>
-          </Router>
-      );
-    }
+          )}
+        </div>
+      </Router>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
---
+++ src/components/sidebar/Icon.jsx
@@ -0,0 +1,3 @@
+// import React from 'react';
+// const Icon = (props) => <img src="" alt=""/>{props.src}!</h1>;
+// export default Icon;
