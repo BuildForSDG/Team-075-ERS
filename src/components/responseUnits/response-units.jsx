c559415d3008a845b9565bd3e801b7ac139c3652
unknown <oseeronlan@gmail.com>
2020-06-16 12:18:46 +0100

    fix:edit victim modal

--- src/components/responseUnits/response-units.jsx
+++ src/components/responseUnits/response-units.jsx
@@ -5,22 +5,21 @@ import Card from '../card/card';
 import './response-units.css';
 
 const ResponseUnits = ({ response }) => {
-  return (
-    response.units.locations.map((unit, index) => (
-        <Card 
-          key={index}
-          name={unit.name}
-          latitude={unit.location.latitude}
-          longitude={unit.location.longitude}
-          imageURL={`https://robohash.org/set_set5/${unit.name}?size=50x50`}
-        />
-      )
-    )
-  );
-}
+  return response.units.locations.map((unit, index) => (
+    <div className="response-homepage">
+      <Card
+        key={index}
+        name={unit.name}
+        latitude={unit.location.latitude}
+        longitude={unit.location.longitude}
+        imageURL={`https://robohash.org/set_set5/${unit.name}?size=50x50`}
+      />
+    </div>
+  ));
+};
 
 const mapStateToProps = (state) => ({
-    response: state.response
+  response: state.response
 });
 
 export default connect(mapStateToProps)(ResponseUnits);
--- src/components/show-victim-info/showVictimInfo.jsx
+++ src/components/show-victim-info/showVictimInfo.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
 import CustomButton from '../custom-button/CustomButton';
-
+import './show-victim-info.css';
 
 const ShowVictimProfile = ({ id, name, phoneNo, userId, lat, lng, status, createdAt, updatedAt }) => (
-  <div className='victim-info'>
-    <p>Id: {id}</p>
-    <br></br>
-    <strong>Name: {name}</strong>
-    <p>Reporter Phone No: <span></span>
-      <a className='victim-tel' href={`tel:${phoneNo}`}>{phoneNo}</a>
+  <div className="victim-info">
+    <p>
+      Name: <span>{name}</span>
     </p>
-    <p>Reporter userid: {userId}</p>
-    <br></br>
-    <p>Latitude: {lat}</p>
-    <p>Longitude: {lng}</p>
-    <br></br>
-    <p>Status: {status}</p><CustomButton>Deploy Personnel</CustomButton>
-    <br></br>
-    <p>Created At: {createdAt}</p>
-    <br></br>
-    <p>Updated At: {updatedAt}</p>
+    <p>
+      Id: <span>{id}</span>{' '}
+    </p>
+    <p>
+      Reporter Phone No:{' '}
+      <span>
+        <a className="victim-tel" href={`tel:${phoneNo}`}>
+          {phoneNo}
+        </a>
+      </span>
+    </p>
+    <p>
+      Reporter userid: <span>{userId}</span>{' '}
+    </p>
+    <p>
+      Latitude: <span>{lat}</span>{' '}
+    </p>
+    <p>
+      Longitude: <span>{lng}</span>{' '}
+    </p>
+    <p>
+      Status: <span>{status}</span>{' '}
+    </p>
+
+    <p>
+      Created At: <span>{createdAt}</span>
+    </p>
+    <p>
+      Updated At: <span>{updatedAt}</span>{' '}
+    </p>
+    <CustomButton id="deploy-button">Deploy Personnel</CustomButton>
   </div>
 );
 
